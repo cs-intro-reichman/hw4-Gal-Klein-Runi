f0962a5c14e0db9433b12f8659ee4ca7ff4f5eb1
Gal Klein <gal.klein@post.runi.ac.il>
2024-12-05 18:10:56 +0200

    try

--- Primes.java
+++ Primes.java
@@ -1,5 +1,32 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[n + 1];
+
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = true;
+        }
+        int p = 2;
+        while (p*p <= n) {
+            for (int i = p; i < arr.length; i++) {
+                if (i % p == 0 && i!=p) {
+                    arr[i] = false;
+                }
+
+            }
+            for (int i = p + 1; i < arr.length; i++) {
+                if (arr[i]) {
+                    p = i;
+                    break;
+                }
+            }
+        }
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] == true) {
+                System.out.println(i);
+            }
+
+        }
     }
 }
